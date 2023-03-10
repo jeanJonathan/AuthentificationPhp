07f232cd8a1b4a0992ab29b6a49d82cdba55a64a
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-10 19:05:27 +0100

    Ajout du formulaire d inscription

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Inscription</title>
+</head>
+<body>
+<h1>Inscription</h1>
+
+<form method="post">
+    <label>Nom :</label><br>
+    <input type="text" name="nom" required><br>
+    <label>Prénom :</label><br>
+    <input type="text" name="prenom" required><br>
+    <label>Adresse e-mail :</label><br>
+    <input type="email" name="email" required><br>
+    <label>Mot de passe :</label><br>
+    <input type="password" name="mdp" required><br>
+    <input type="submit" name="inscription" value="S'inscrire">
+</form>
+</body>
+</html>
