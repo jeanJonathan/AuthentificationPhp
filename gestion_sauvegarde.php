f05aa76160093daa2f154917e0f3d338ec6e55e4
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-11 15:37:04 +0100

    Ajout du formulaire du fichier gestion_sauvegarde

---
+++ gestion_sauvegarde.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion des sauvegardes</title>
+</head>
+<body>
+<h1>Gestion des sauvegardes</h1>
+<p>Vous pouvez sauvegarder et restaurer la base de données à partir de cette page.</p>
+
+<form action="gestion_sauvegarde.php" method="post">
+    <input type="submit" name="sauvegarder" value="Sauvegarder la base de données">
+    <input type="submit" name="restaurer" value="Restaurer la base de données">
+</form>
+
+</body>
+</html>
