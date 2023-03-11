0485293b4f359ef762f6231a1c2a6aaa19ff03d4
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-11 13:24:41 +0100

    Ajout du corp du fichier acces_refuse

---
+++ acces_refuse.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Accès refusé</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>Accès refusé</h1>
+<p>Vous n'avez pas les permissions nécessaires pour accéder à cette page.</p>
+<a href="index.php">Retour à l'accueil</a>
+</body>
+</html>
